a291ce3604ef8408456a284d6eae91a9b79efbb2
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2022-12-02 11:09:48 -0600

    update tests

--- src/test/kotlin/com/testing/a/thing/gplib/LibraryTest.kt
+++ src/test/kotlin/com/testing/a/thing/gplib/LibraryTest.kt
@@ -1,17 +1,16 @@
 package com.testing.a.thing.gplib
 
-import testing.`this`.gplib.Library
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class LibraryTest {
     @Test fun someLibraryMethodReturnsTrue() {
-        val classUnderTest = Library()
+        val classUnderTest = GPLibrary()
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
     }
 
     @Test fun anotherLibraryMethodTest() {
-        assertEquals(Library().anotherLibraryMethod(), "Yep, worked.")
+        assertEquals(GPLibrary().anotherLibraryMethod(), "Yep, worked.")
     }
 }
